23e2167b6c403220e4f5caee7a79c7be0b8f983c
Rockybhai21rs <rockingstar.21rs@gmail.com>
2024-11-27 13:18:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7481226930:AAFpsEUkjpHA9bUhEsMBvvFZTubjdcsK3Ys")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 27317669))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "11b88c331c5d44fde57cf91de1a2156b")
 
     AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
 
